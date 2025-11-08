08f44c41074e3c41520ae082f493b989beb80e0f
Winnie Seroney <s.ayioka@alustudent.com>
2025-11-08 23:05:15 +0300

    Change Django settings module to pregnancy_tracker

--- pregnancy_tracker/wsgi.py
+++ pregnancy_tracker/wsgi.py
@@ -11,7 +11,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 # Set the default Django settings module
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lindamama.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pregnancy_tracker.settings')
 
 # Get the WSGI application
 application = get_wsgi_application()
