8350cce793b0fbfe329ce3685bfca77da10e6a3b
ayioka <s.ayioka@alustudent.com>
2025-11-08 12:57:07 +0300

    Refactor URL patterns to use TemplateView for home

--- pregnancy_tracker/urls.py
+++ pregnancy_tracker/urls.py
@@ -1,18 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
-from pregnancy.views import home
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home, name='home'),
-    path('dashboard/', include('dashboard.urls')),
-    path('users/', include('users.urls')),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('pregnancy/', include('pregnancy.urls')),
-    path('appointments/', include('appointments.urls')),
-    path('messaging/', include('messaging.urls')),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
